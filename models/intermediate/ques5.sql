019f18af07200f3a4b7bf3bfedbf6da0d4552f8c
Arly Raymundo <raymundoarly15@gmail.com>
2023-09-11 05:51:53 +0000

    added top 10 to int table

--- models/intermediate/ques5.sql
+++ models/intermediate/ques5.sql
@@ -1,5 +1,4 @@
 
-
 WITH 
 
 tv_shows AS (
@@ -9,3 +8,10 @@ movies AS (
     SELECT * FROM {{ref ('stg_movies') }}
 ),
 
+top_10 AS (
+    SELECT * FROM {{ ref ('stg_top_10')}}
+),
+
+raw_credits  AS (
+    SELECT * FROM {{ ref ('stg_raw_credits')}}
+),
